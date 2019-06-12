f10a439165304b586b744c5bbfdda02ec6c4f371
Marc Dwyer <marcdwyer1738@gmail.com>
2019-06-11 22:57:18 -0500

    parent nav div is at a set width, anmimation div now expands with animation

--- src/components/Navigator/nav.tsx
+++ src/components/Navigator/nav.tsx
@@ -19,47 +19,49 @@ const Nav = (props: Props) => {
     })
 
     return (
-        <animated.div className="master-nav" style={sprProps}>
-            <div>
-                <img src="https://s3.us-east-2.amazonaws.com/fetchappbucket/port/me.jpg" alt="me" />
-                <div className="inner-text">
-                    <h1>Marc Dwyer</h1>
-                    <h3>Web Developer</h3>
-                </div>
-                <div className="app-links">
-                    {Links.map((item: string) => {
-                        const lowerStr = item.toLowerCase()
-                        const path = lowerStr === "home" ? "/" : lowerStr
-                        return (
-                            <Link
-                                style={props.route === lowerStr || lowerStr === "home" && !props.route ? {color: "rgba(255,255,255,1)"} : {}}
-                                key={item}
-                                to={path}
-                                className="linker"
-                            >
-                                {item}
-                            </Link>
-                        )
-                    })}
-                </div>
-                <div className="links">
-                    <div className="sublinks">
-                        <a
-                            href="https://github.com/MarcDwyer?tab=repositories"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="fa fa-github"
-                        />
-                        <a
-                            href="https://www.linkedin.com/in/marc-dwyer-53087339/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="fa fa-linkedin"
-                        />
+        <div className="master-nav">
+            <animated.div className="animated-nav" style={sprProps}>
+                <div className="subDiv">
+                    <img src="https://s3.us-east-2.amazonaws.com/fetchappbucket/port/me.jpg" alt="me" />
+                    <div className="inner-text">
+                        <h1>Marc Dwyer</h1>
+                        <h3>Web Developer</h3>
+                    </div>
+                    <div className="app-links">
+                        {Links.map((item: string) => {
+                            const lowerStr = item.toLowerCase()
+                            const path = lowerStr === "home" ? "/" : lowerStr
+                            return (
+                                <Link
+                                    style={props.route === lowerStr || (lowerStr === "home" && !props.route) ? { color: "rgba(255,255,255,1)" } : {}}
+                                    key={item}
+                                    to={path}
+                                    className="linker"
+                                >
+                                    {item}
+                                </Link>
+                            )
+                        })}
+                    </div>
+                    <div className="links">
+                        <div className="sublinks">
+                            <a
+                                href="https://github.com/MarcDwyer?tab=repositories"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="fa fa-github"
+                            />
+                            <a
+                                href="https://www.linkedin.com/in/marc-dwyer-53087339/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="fa fa-linkedin"
+                            />
+                        </div>
                     </div>
                 </div>
-            </div>
-        </animated.div>
+            </animated.div>
+        </div>
     )
 }
 
--- src/components/Projects/projects.tsx
+++ src/components/Projects/projects.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './projects.scss'
-
+import { animated, useSpring } from 'react-spring'
 import { projects } from '../../data/data'
 
 const Projects = () => {
@@ -9,16 +9,20 @@ const Projects = () => {
             {projects.map((item, i) => {
                 const int = i + 2
                 return (
-                    <div className="project" key={item.url}>
+                    <animated.div className="project" key={item.url}>
                         <div className={`proj ${int % 2 !== 0 ? "odd" : "even"}`}>
                             <div className="body">
-                                <h2>{item.title}</h2>
+                                <h2>{item.title}
+                                    {item.ocean && (
+                                        <small> -- hosted on DigitalOcean</small>
+                                    )}
+                                </h2>
                                 <span>Stack: {item.stack}</span>
                                 <p>{item.details}</p>
                             </div>
                             <img src={item.url} alt="application" />
                         </div>
-                    </div>
+                    </animated.div>
                 )
             })}
         </div>
