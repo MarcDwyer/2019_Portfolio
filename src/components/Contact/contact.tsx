606403493e39e33253a333d8058b455f4966e99f
Marc Dwyer <marcdwyer1738@gmail.com>
2020-02-24 21:34:12 -0600

    some media queries

--- src/components/Contact/contact.tsx
+++ src/components/Contact/contact.tsx
@@ -1,31 +1,29 @@
-import React from 'react'
-import { useSpring, animated } from 'react-spring'
+import React from "react";
+import { useSpring, animated } from "react-spring";
 
-
-import './contact.scss'
+import "./contact.scss";
 
 const Contact = () => {
+  const subProps = useSpring({
+    transform: "translateX(0%)",
+    opacity: 1,
+    from: { transform: "translateX(-100%)", opacity: 0 },
+    delay: 100
+  });
 
-    const subProps = useSpring({
-        transform: "translateX(0%)",
-        opacity: 1,
-        from: { transform: "translateX(-100%)", opacity: 0 },
-        delay: 100
-    })
-
-    return (
-        <div className="master contact">
-            <animated.div className="sub-contact card" style={subProps}>
-                <span>Marc Dwyer</span>
-                <div className="contact-info">
-                    <div className="email">
-                    <i className="fa fa-envelope" />
-                    <a href="mailto:marcdwyer1738@gmail.com">marcdwyer1738@gmail.com</a>
-                    </div>
-                </div>
-            </animated.div>
+  return (
+    <div className="contact">
+      <animated.div className="sub-contact card" style={subProps}>
+        <span>Marc Dwyer</span>
+        <div className="contact-info">
+          <div className="email">
+            <i className="fa fa-envelope" />
+            <a href="mailto:marcdwyer1738@gmail.com">marcdwyer1738@gmail.com</a>
+          </div>
         </div>
-    )
-}
+      </animated.div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
